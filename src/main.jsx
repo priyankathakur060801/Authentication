fe815440e1f08881bb450c43c4e2355443337a2e
priyankathakur06 <priyankathakur060801@gmail.com>
2025-09-19 23:09:03 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppContextProvider } from './context/AppContext.jsx'
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/">
     <AppContextProvider>
       <App />
     </AppContextProvider>
